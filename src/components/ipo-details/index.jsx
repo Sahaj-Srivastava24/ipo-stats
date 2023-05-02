29470f7c2084adf975cf48b2486ac5b3d8c0fb28
shouryade <shourya.de12@gmail.com>
2023-05-02 21:28:26 +0530

    tsx-jsx
    Signed-off-by: shouryade <shourya.de12@gmail.com>

--- src/components/ipo-details/index.jsx
+++ src/components/ipo-details/index.jsx
@@ -3,15 +3,18 @@ import CompanyData from "./ipo-details-1.json";
 export default function IPOTables(props) {
   let companyName = props.name;
   let companyDetails;
+  let deets;
   for (let i = 0; i < CompanyData.companies.length; i++) {
     if (CompanyData.companies[i].name === companyName) {
       companyDetails = CompanyData.companies[i].ipo_details;
+      deets = CompanyData.companies[i];
       break;
     }
   }
 
   // display the IPO details of the company
   if (companyDetails) {
+    console.log(deets);
     console.log(`IPO Lot Size: ${companyDetails.lot_size}`);
     console.log(`IPO Price Range: ${companyDetails.price}`);
     // display other IPO details as needed

4b319c449d586d9ba3a8ef5cce85332e528e8e50
shouryade <shourya.de12@gmail.com>
2023-05-04 10:43:30 +0530

    live listed up down
    Signed-off-by: shouryade <shourya.de12@gmail.com>

--- src/views/ipo_list/index.tsx
+++ src/views/ipo_list/index.tsx
@@ -1,14 +1,21 @@
-import IPOCardWrapper from "components/components/ipo_card_wrapper"
-import {ListedIPOs as listedIPO} from "components/entities/listed-ipo"
-import {LiveIPOs as liveIPO} from "components/entities/live-ipo"
-
+import IPOCardWrapper from "components/components/ipo_card_wrapper";
+import { ListedIPOs as listedIPO } from "components/entities/listed-ipo";
+import { LiveIPOs as liveIPO } from "components/entities/live-ipo";
 
 export default function IPOList() {
   return (
-    <div className="bg-backgroundDark w-full">
-      <IPOCardWrapper heading={'Listed IPOs'} subheading={'IPOs that are currently listed on exchanges.'} ipos={listedIPO} />
+    <div className="w-full bg-backgroundDark">
+      <IPOCardWrapper
+        heading={"Live IPOs"}
+        subheading={"IPOs that are currently live for the subscription."}
+        ipos={liveIPO}
+      />
+      <IPOCardWrapper
+        heading={"Listed IPOs"}
+        subheading={"IPOs that are currently listed on exchanges."}
+        ipos={listedIPO}
+      />
       {/* eslint-disable-next-line */}
-      <IPOCardWrapper heading={'Live IPOs'} subheading={'IPOs that are currently live for the subscription.'} ipos={liveIPO} />
     </div>
-  )
+  );
 }
